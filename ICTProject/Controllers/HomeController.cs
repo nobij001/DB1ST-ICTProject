01f13debf0faae508a01aa0dcc08eae10098c016
nobij001 <nobij001@mymail.unisa.edu.au>
2017-09-05 10:30:28 +0930

    Add project files.

---
+++ ICTProject/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ICTProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        [AllowAnonymous] // This is what you see when you're not logged in.
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult TestNav()
+        {
+            return View();
+        }
+    }
+}
