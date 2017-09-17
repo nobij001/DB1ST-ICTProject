10dc0d4a52bea02a54806d50ef2c621565ed202a
JordanNguyen <Admin@RIKKACHAN>
2017-09-17 19:49:02 +0930

    Made all the dropdowns. With pages for each report and data entry..
    I think? if any are missing i can add them

---
+++ ICTProject/Controllers/DropReportsController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ICTProject.Controllers
+{
+    public class DropReportsController : Controller
+    {
+        // GET: Drop3
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult attendanceTracker()
+        {
+            return View();
+        }
+
+        public ActionResult intPerformance()
+        {
+            return View();
+        }
+        public ActionResult nightlyPerformance()
+        {
+            return View();
+        }
+        public ActionResult top10country()
+        {
+            return View();
+        }
+        public ActionResult top10World()
+        {
+            return View();
+        }
+        public ActionResult topGunsNightly()
+        {
+            return View();
+        }
+        public ActionResult weeklyBestRegion()
+        {
+            return View();
+        }
+        public ActionResult weeklyPerformance()
+        {
+            return View();
+        }
+        public ActionResult weeklySalesStats()
+        {
+            return View();
+        }
+    }
+}
